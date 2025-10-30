93664e1b64169cc66540fde134c3f7d64e165fe9
anivnssb <anvinssb@gmail.com>
2025-10-30 09:56:07 +0530

    removing mui imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-import Button from "@mui/material/Button";
-import styles from "./page.module.css";
 import AddTaskForm from "../components/AddTaskForm";
-import { Container } from "@mui/material";
 import StoreProvider from "../store/StoreProvider";
 import TaskList from "../components/TaskList";
 
 export default function Home() {
   return (
     <StoreProvider>
-      <Container sx={{ marginTop: "100px" }}>
-        <AddTaskForm />
-        <TaskList />
-      </Container>
+      <AddTaskForm />
+      <TaskList />
     </StoreProvider>
   );
 }
